5e120735bf3f399f8a1cdb181f1c11fd210f33a8
Snex-Thadeus <lwande94@gmail.com>
2020-09-09 18:04:34 +0300

    Order summary functionality

--- duka/urls.py
+++ duka/urls.py
@@ -5,6 +5,7 @@ from .views import (
     HomeView,
     add_to_cart,
     remove_from_cart,
+    remove_single_item_from_cart,
     OrderSummaryView
 )
 
@@ -17,5 +18,6 @@ urlpatterns = [
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart')
 ]
