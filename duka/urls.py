73c15892d8abe363a0d502f8bc5b6b65a1fea77a
Snex-Thadeus <lwande94@gmail.com>
2020-09-10 02:13:08 +0300

    Payment functionality using stripe

--- duka/urls.py
+++ duka/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     ItemDetailView,
     CheckoutView,
+    PaymentView,
     HomeView,
     add_to_cart,
     remove_from_cart,
@@ -15,6 +16,7 @@ app_name = 'duka'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
--- soko/settings.py
+++ soko/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cm!)lehg!me-e3ughebq0t1wokv7%@ob*cdi72_h553i=3t381'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+STRIPE_SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
