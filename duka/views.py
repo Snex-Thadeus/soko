e2955b485b1ac60c1ac7a1c3f2b2c9c1fc0f56b5
Snex-Thadeus <lwande94@gmail.com>
2020-09-10 18:05:41 +0300

    Refunds functionality

--- duka/views.py
+++ duka/views.py
@@ -6,11 +6,17 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.views.generic import ListView, DetailView, View
-from .forms import CheckoutForm, CouponForm
+from .forms import CheckoutForm, CouponForm, RefundForm
 from .models import *
 
 import stripe
+import random
+import string
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def create_ref_code():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
 
 
 def products(request):
@@ -113,6 +119,7 @@ class PaymentView(View):
 
             order.ordered = True
             order.payment = payment
+            order.ref_code = create_ref_code()
             order.save()
 
             messages.success(self.request, "Your order was successful")
@@ -284,3 +291,37 @@ class AddCouponView(View):
                 messages.info(self.request, "You do not have an active order.")
                 return redirect("duka:checkout")
 
+
+class RequestRefundView(View):
+    def get(self, *args, **kwargs):
+        form = RefundForm()
+        context = {
+            'form': form
+        }
+        return render(self.request, "request_refund.html", context)
+
+    def post(self, *args, **kwargs):
+        form = RefundForm(self.request.POST)
+        if form.is_valid():
+           ref_code = form.cleaned_data.get('ref_code')
+           message = form.cleaned_data.get('message')
+           email = form.cleaned_data.get('email')
+            # edit the order
+           try:
+               order = Order.objects.get(ref_code=ref_code)
+               order.refund_requested = True
+               order.save()
+
+               #  store the refund
+               refund = Refund()
+               refund.order = order
+               refund.reason = message
+               refund.email = email
+               refund.save()
+
+               messages.info(self.request, "Your request was received")
+               return redirect("duka:request-refund")
+
+           except ObjectDoesNotExist:
+            messages.info(self.request, "This order does not exists")
+            return redirect("duka:request-refund")
